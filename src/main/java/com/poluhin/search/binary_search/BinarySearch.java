62f87741f94a9e37398de66aa434c2af42f03731
Сергей Полухин <s.poluh@ro.ru>
2019-01-17 23:50:09 +0300

    Craete loop-binary search (DK why, but possible for the lulz)

--- src/main/java/com/poluhin/search/binary_search/BinarySearch.java
+++ src/main/java/com/poluhin/search/binary_search/BinarySearch.java
@@ -26,6 +26,21 @@ public class BinarySearch<E extends Comparable<E>> implements Search<E> {
         return middle;
     }
 
+    private int loopSearch(E[] a, E e) {
+        var low = 0;
+        var high = a.length - 1;
+
+        while (low <= high) {
+            var middle = (low + high) >>> 1;
+            var cmp = a[middle].compareTo(e);
+
+            if (cmp < 0) low = middle + 1;
+            if (cmp > 0) high = middle - 1;
+            if (cmp == 0) return middle;
+        }
+        return -1;
+    }
+
     private int simplePositions(E[] a, E e, int start, int end) {
         if (start > end || start < 0 || end > a.length - 1 || a.length == 0) return NOT_FOUND;
         if (a.length == 1) return 0;
